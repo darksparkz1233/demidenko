d918b00896506addf29ff99910fb8ad1212591f1
Pavel <66474636+darksparkz1233@users.noreply.github.com>
2023-02-09 03:35:24 +0300

    Add files via upload

---
+++ src/components/CarouselVue.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-carousel cycle :show-arrows="false">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'http://belgosohota.by/wp-content/uploads/2018/09/15_06_05_05_13_sooruzenia.jpg',
+          },
+          {
+            src: 'https://www.plho.by/images/hunt/01vasilevichi/vasilevishi01.jpg',
+          },
+          {
+            src: 'https://www.plho.by/images/hunt/01vasilevichi/vasilevishi04.jpg',
+          },
+          {
+            src: 'https://www.plho.by/images/hunt/01vasilevichi/vasilevishi02.jpg',
+          },
+          {
+            src: 'http://belgosohota.by/wp-content/uploads/2018/09/18_08_29_03_34_oxdom.jpg'
+          }
+        ],
+      }
+    },
+  }
+</script>
+<style >
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card>
+      <v-tabs
+        color="deep-purple accent-4"
+        right
+      >
+        <v-tab :href="tab-1">Landscape</v-tab>
+        <v-tab :href="tab-2">City</v-tab>
+      </v-tabs>
+          <v-tabs-items v-model="tab">
+              <v-tab-item
+                  v-for="i in 3"
+                  :key="i"
+                  :value="'tab-' + i">
+                  <img src="../img/1.jpg" alt="">
+              </v-tab-item>
+          </v-tabs-items>
+    </v-card>
+  </template>
+  <script>
+  export default {
+    name: 'App',
+    components: {
+    },
+    data: () => ({
+        paths: [
+            '../img/1.jpg',
+            '../img/2.jpg',
+            '../img/3.jpg',
+            '../img/4.jpg'
+        ]
+    }),
+  };
+  </script>
+  
+  <style>
+  body {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  </style>
